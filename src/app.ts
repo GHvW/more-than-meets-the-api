0156859cb266923b06d54a2c033af6b2467f7156
Garrett van Wageningen <ghvanw283@gmail.com>
2019-05-05 22:19:51 -0500

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+// import { Pool } from "pg";
+import express from "express";
+// const express = require("express");
+// import { PgConfig } from "../secrets";
+
+// const pool = new Pool(PgConfig);
+
+
+const SITE_URL = "http://localhost:1337/api";
+
+
+const app = express();
+const router = express.Router();
+
+router.use("/", (_req, res) => {
+  res.send({
+    transformers: `${SITE_URL}/transformers/`
+  });
+});
+
+app.use("/api", router);
+
+module.exports = app;
---
+++ src/transformer/model.ts
@@ -0,0 +1,10 @@
+
+export class Transformer {
+  name: string;
+  allegiance: string;
+
+  constructor(name: string, allegiance: string) {
+    this.name = name;
+    this.allegiance = allegiance;
+  }
+}
