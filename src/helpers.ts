a52b5f2640e567cfd1649a0ae8d57fda46a5d97b
Garrett van Wageningen <ghvanw283@gmail.com>
2019-05-25 19:40:19 -0500

    curry asLinks and export applied versions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,15 @@
+import { SITE_URL } from "./app";
 
-export function asLinks(ids: number[], resource: string): string[] {
-  return ids.map(id => `${resource}/${id}`)
+export function asLinks(resource: string): (ids: number[]) => string[] {
+  return (ids) => {
+    return ids[0] !== null
+    ? ids.map(id => `${resource}/${id}`)
+    : [];
+  }
 }
+
+export const asAltModeLinks = asLinks(`${SITE_URL}/altmodes`);
+export const asTransformerLinks = asLinks(`${SITE_URL}/transformers`);
+export const asSeriesLinks = asLinks(`${SITE_URL}/series`);
+export const asAbilityLinks = asLinks(`${SITE_URL}/abilities`);
+export const asWeaponLinks = asLinks(`${SITE_URL}/weapons`);
