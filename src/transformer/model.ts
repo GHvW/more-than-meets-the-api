bf87e9fab3d4626fed950322974f1c211b1c072c
Garrett van Wageningen <ghvanw283@gmail.com>
2019-06-12 20:45:41 -0500

    refactor for groups field

--- src/transformer/model.ts
+++ src/transformer/model.ts
@@ -8,6 +8,7 @@ export class Transformer {
   faction: string;
   alt_modes: string[];
   weapons: string[];
+  groups: string[];
   abilities: string[];
   appearances: string[];
   created: string;
@@ -17,6 +18,7 @@ export class Transformer {
     name: string, 
     faction: string, 
     alt_modes: number[],
+    groups: number[],
     weapons: number[],
     abilities: number[],
     appearances: number[],
@@ -26,6 +28,7 @@ export class Transformer {
     this.name = name;
     this.faction = faction;
     this.alt_modes = asLinks(`${SITE_URL}`)(`altmodes`)(alt_modes);
+    this.groups = asLinks(`${SITE_URL}`)(`groups`)(groups);
     this.weapons = asLinks(`${SITE_URL}`)(`weapons`)(weapons);
     this.abilities = asLinks(`${SITE_URL}`)(`abilities`)(abilities);
     this.appearances = asLinks(`${SITE_URL}`)(`installments`)(appearances);
